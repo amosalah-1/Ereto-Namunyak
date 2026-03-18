93e32ede110d65ad3f8d660005480e92942660d8
amosalah-1 <amosalah092@gmail.com>
2026-03-18 22:36:08 +0300

    Fix 405 Error by configuring Vercel builds

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Debug Middleware: Log all requests to Vercel Function Logs
+app.use((req, res, next) => {
+    console.log(`[${req.method}] ${req.url}`);
+    next();
+});
+
 // Serve static files (HTML, CSS, JS, Images)
 app.use(express.static(path.join(__dirname)));
 
